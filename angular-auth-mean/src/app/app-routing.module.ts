bb7aabacf01cb60ce65c954b6411062f1097453b
Muhammad Aqib <muhammad.aqib@vantibolli.com>
2018-11-30 15:34:53 +0500

    git status -s

--- angular-auth-mean/src/app/app-routing.module.ts
+++ angular-auth-mean/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './compunent/home/home.component';
+import { RegisterComponent } from './compunent/register/register.component';
+import { LoginComponent } from './compunent/login/login.component';
+import { DashboardComponent } from './compunent/dashboard/dashboard.component';
+import { ProfileComponent } from './compunent/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},{
+path:'register',
+component:RegisterComponent
+},{
+path:'login',
+component:LoginComponent
+},{
+  path:'dashboard',
+  component:DashboardComponent
+  },{
+    path:'profile',
+    component:ProfileComponent
+    }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-auth-mean/src/app/app.module.ts
+++ angular-auth-mean/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './compunent/login/login.component';
+import { NavbarComponent } from './compunent/navbar/navbar.component';
+import { RegisterComponent } from './compunent/register/register.component';
+import { HomeComponent } from './compunent/home/home.component';
+import { ProfileComponent } from './compunent/profile/profile.component';
+import { DashboardComponent } from './compunent/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
