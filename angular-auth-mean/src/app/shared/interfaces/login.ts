780019280d39e439a8f859a204b5b17761eaeb61
Muhammad Aqib <muhammad.aqib@vantibolli.com>
2018-12-13 10:56:27 +0500

    change interface

--- angular-auth-mean/src/app/shared/interfaces/login.ts
+++ angular-auth-mean/src/app/shared/interfaces/login.ts
@@ -1,11 +1,15 @@
+interface Iuser{
+        id:string,
+        name :string,
+        username:string,
+        email:string
+     }
+
+
+
+
 export interface Ilogin{
 success:boolean,
 token:string,
- user:{
-    id:string,
-    name :string,
-    username:string,
-    email:string
-    
-}
+ user:Iuser
 }
